8a79320feddeb3ed23024fe6d95decbde5f03908
Jakku Night <r101g117l117@gmail.com>
2021-04-17 23:40:38 +0000

    Proyecto base

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from "./components/Card";
+import Clock from "./components/Clock";
+
+/*
+function tick() {
+    const element = (
+        <div>
+            <Card user="Santiago"/>
+            <Clock/>
+        </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
+*/
+
+
